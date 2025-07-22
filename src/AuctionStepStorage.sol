10297c93566da4d1efe1f14f824945543d2c0fbe
Eric Zhong <eric.zhong@uniswap.org>
2025-07-22 11:05:19 -0400

    natspec and nits

--- src/AuctionStepStorage.sol
+++ src/AuctionStepStorage.sol
@@ -5,26 +5,26 @@ import {IAuctionStepStorage} from './interfaces/IAuctionStepStorage.sol';
 import {AuctionStep, AuctionStepLib} from './libraries/AuctionStepLib.sol';
 import {SSTORE2} from 'solady/utils/SSTORE2.sol';
 
+/// @title AuctionStepStorage
+/// @notice Abstract contract to store and read information about the auction issuance schedule
 abstract contract AuctionStepStorage is IAuctionStepStorage {
     using AuctionStepLib for *;
     using SSTORE2 for *;
 
-    error InvalidAuctionDataLength();
-    error InvalidBps();
-    error InvalidEndBlock();
-
+    /// @notice The size of a uint64 in bytes
+    uint256 public constant UINT64_SIZE = 8;
     /// @notice The block at which the auction starts
     uint64 public immutable startBlock;
     /// @notice The block at which the auction ends
     uint64 public immutable endBlock;
+    /// @notice Cached length of the auction steps data provided in the constructor
+    uint256 private immutable _length;
 
+    /// @notice The address pointer to the contract deployed by SSTORE2
     address public pointer;
     /// @notice The word offset of the last read step in `auctionStepsData` bytes
     uint256 public offset;
-    uint256 public constant UINT64_SIZE = 8;
-
-    uint256 private immutable _length;
-
+    /// @notice The current active auction step
     AuctionStep public step;
 
     constructor(bytes memory _auctionStepsData, uint64 _startBlock, uint64 _endBlock) {
@@ -34,18 +34,21 @@ abstract contract AuctionStepStorage is IAuctionStepStorage {
         _length = _auctionStepsData.length;
 
         address _pointer = _auctionStepsData.write();
-        require(_pointer != address(0), 'Invalid pointer');
+        if (_pointer == address(0)) revert InvalidPointer();
 
         _validate(_pointer);
         pointer = _pointer;
     }
 
+    /// @notice Validate the data provided in the constructor
+    /// @dev Checks that the contract was correctly deployed by SSTORE2 and that the total bps and blocks are valid
     function _validate(address _pointer) private view {
         bytes memory _auctionStepsData = _pointer.read();
         if (
             _auctionStepsData.length == 0 || _auctionStepsData.length % UINT64_SIZE != 0
                 || _auctionStepsData.length != _length
         ) revert InvalidAuctionDataLength();
+
         // Loop through the auction steps data and check if the bps is valid
         uint256 sumBps;
         uint64 sumBlockDelta;

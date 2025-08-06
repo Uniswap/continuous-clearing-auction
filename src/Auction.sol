41667f82e7eca3fb0abbafd893655f4b1c3cf0df
Eric Zhong <eric.zhong@uniswap.org>
2025-08-06 15:02:39 -0400

    move transfer out of branch

--- src/Auction.sol
+++ src/Auction.sol
@@ -318,9 +318,9 @@ contract Auction is PermitSingleForwarder, IAuction, TickStorage, AuctionStepSto
             bid.tokensFilled = tokensFilled;
             bid.withdrawnBlock = uint64(block.number);
             _updateBid(bidId, bid);
-            
-            currency.transfer(bid.owner, refund);
         }
+        
+        currency.transfer(bid.owner, refund);
         
         emit BidWithdrawn(bidId, bid.owner);
     }

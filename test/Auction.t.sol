413e77b2491b47141fd53c843865407ebd829afc
Eric Zhong <eric.zhong@uniswap.org>
2025-09-21 15:17:48 -0400

    resolve conflicts

--- test/Auction.t.sol
+++ test/Auction.t.sol
@@ -1525,7 +1525,7 @@ contract AuctionTest is AuctionBaseTest {
 
         // The amount cleared will be the total cleared / the _numberOfBids
         Checkpoint memory lastCheckpoint = auction.latestCheckpoint();
-        uint128 amountClearedPerBid = lastCheckpoint.totalCleared / _numberOfBids;
+        uint256 amountClearedPerBid = ValueX7.unwrap(lastCheckpoint.totalCleared.divUint256(_numberOfBids));
 
         vm.roll(auction.claimBlock());
         vm.expectEmit(true, true, true, true);

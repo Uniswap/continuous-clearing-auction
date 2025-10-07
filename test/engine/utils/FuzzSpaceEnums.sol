11cf517d6f518844c426c34f775ab95e0a80348b
Eric Zhong <eric.zhong@uniswap.org>
2025-10-07 17:18:12 -0400

    add enums

---
+++ test/engine/utils/FuzzSpaceEnums.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+enum AuctionPhaseSpace {
+    NotYetStarted,
+    FirstBlock,
+    Early,
+    Middle,
+    LastBlock,
+    Ended
+}
+
+enum TotalSupplySpace {
+    Low,
+    High
+}
+
+enum TokenDecimalsSpace {
+    Low,
+    High,
+    Standard
+}
+
+enum CurrencyTypeSpace {
+    Native,
+    ERC20
+}
+
+enum CurrencyDecimalsSpace {
+    Low,
+    High,
+    Standard
+}
+
+enum CurrencyAmountSpace {
+    Low,
+    High,
+    Standard
+}
+
+enum FloorPriceSpace {
+    Zero,
+    OneWei,
+    VeryHigh,
+    VeryLow,
+    AtMaxSqrtPrice
+}
+
+enum TickSpacingSpace {
+    Zero,
+    OneWei,
+    OneBasisPoint,
+    LessThanFloorPrice,
+    EqualToFloorPrice,
+    BiggerThanFloorPrice,
+    Max
+}
+
+enum GraduatedSpace {
+    No,
+    Yes
+}
+
+enum SubscriptionStatusSpace {
+    NotSubscribed,
+    SmallSubscription,
+    AlmostSubscribed,
+    ExactlySubscribed,
+    BarelyOversubscribed,
+    MassivelyOversubscribed
+}
+
+enum ExistingBidsSpace {
+    None,
+    One,
+    Multiple,
+    Many
+}
+
+enum BidSizeSpace {
+    Zero,
+    OneWei,
+    BidMinimum,
+    Small,
+    Medium,
+    Large,
+    Huge
+}
+
+enum BidPriceSpace {
+    Zero,
+    OneWei,
+    AtClearingPrice,
+    OneTickAboveClearing,
+    ManyTicksAboveClearing,
+    Max
+}
+
+enum RemainingSupplySpace {
+    None,
+    All
+}
+
+enum ActionSpace {
+    SubmitBid,
+    SubmitBidWithHint,
+    ExitBid,
+    ClaimTokens,
+    Checkpoint
+}
+
+enum EmissionRateSpace {
+    Zero,
+    Small,
+    Normal,
+    Max
+}
+
+enum EmissionDurationSpace {
+    SingleBlock,
+    Short,
+    Normal,
+    Long
+}
+
+enum NumberOfStepsSpace {
+    Zero,
+    Small,
+    Normal,
+    Max
+}
+
+enum SenderSpace {
+    NewBidder,
+    RepeatBidder
+}
+
+enum EnvironmentSpace {
+    Default
+}

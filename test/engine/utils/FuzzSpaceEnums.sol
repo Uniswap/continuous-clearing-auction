821b298142ac10b80cef8f2dcc2c1e12151bc8ba
mgretzke <m.gretzke@vimage.de>
2025-10-09 16:32:57 -0400

    Default FuzzGenerator path

--- test/engine/utils/FuzzSpaceEnums.sol
+++ test/engine/utils/FuzzSpaceEnums.sol
@@ -10,156 +10,191 @@ enum AuctionParametersSpace {
     FundsRecipientSpace
 }
 
+/// @dev The current time phase of the auction
 enum AuctionPhaseSpace {
-    NotYetStarted,
-    FirstBlock,
-    Early,
-    Middle,
-    LastBlock,
-    Ended
+    // NotYetStarted,
+    FirstBlock // first block at which the auction is active
+        // Early,
+        // Middle,
+        // LastBlock,
+        // Ended
+
 }
 
+/// @dev The total supply of the auctioned tokens
 enum TotalSupplySpace {
-    Low,
+    // Low,
     High
 }
 
+/// @dev The number of decimals of the auctioned token
 enum TokenDecimalsSpace {
-    Low,
-    High,
-    Standard
+    // Low,
+    // High,
+    Standard // 18 decimals
+
 }
 
+/// @dev The type of currency being raised in the auction
 enum CurrencyTypeSpace {
-    Native,
-    ERC20
+    Native // ETH
+        // ERC20
+
 }
 
+/// @dev The number of decimals of the currency token
 enum CurrencyDecimalsSpace {
-    Low,
-    High,
-    Standard
+    // Low,
+    // High,
+    Standard // 18 decimals
+
 }
 
-enum CurrencyAmountSpace {
-    Low,
-    High,
-    Standard
-}
-
+/// @dev The minimum price of the auctioned token
 enum FloorPriceSpace {
-    Zero,
-    OneWei,
-    VeryHigh,
-    VeryLow,
-    AtMaxSqrtPrice
+    // Zero,
+    // OneWei,
+    // VeryHigh,
+    VeryLow // a very low price as the floor price
+        // AtMaxSqrtPrice
+
 }
 
+/// @dev The tick spacing of the auction, adding granularity to the bid prices
 enum TickSpacingSpace {
-    Zero,
-    OneWei,
-    OneBasisPoint,
-    LessThanFloorPrice,
-    EqualToFloorPrice,
-    BiggerThanFloorPrice,
-    Max
+    // Zero,
+    // OneWei,
+    OneBasisPoint // 1 basis point
+        // LessThanFloorPrice,
+        // EqualToFloorPrice,
+        // BiggerThanFloorPrice,
+        // Max
+
 }
 
+/// @dev Is the final price higher then the floor price
 enum GraduatedSpace {
-    No,
-    Yes
+    No // the final price is not higher then the floor price
+        // Yes
+
 }
 
+/// @dev The amount of current token demand compared to the current supply
 enum SubscriptionStatusSpace {
-    NotSubscribed,
-    SmallSubscription,
-    AlmostSubscribed,
-    ExactlySubscribed,
-    BarelyOversubscribed,
-    MassivelyOversubscribed
+    NotSubscribed // no previous bids
+        // SmallSubscription,
+        // AlmostSubscribed,
+        // ExactlySubscribed,
+        // BarelyOversubscribed,
+        // MassivelyOversubscribed
+
 }
 
+/// @dev The number of existing bids in the auction
 enum ExistingBidsSpace {
-    None,
-    One,
-    Multiple,
-    Many
+    None // no existing bids
+        // One,
+        // Multiple,
+        // Many
+
 }
 
+/// @dev The amount of currency being spent on the bid
 enum BidSizeSpace {
-    Zero,
-    OneWei,
-    BidMinimum,
-    Small,
-    Medium,
-    Large,
-    Huge
+    // Zero,
+    // OneWei,
+    BidMinimum // the minimum amount required to submit a bid
+        // Small,
+        // Medium,
+        // Large,
+        // Huge
+
 }
 
+/// @dev The amount a user is willing to pay per token in a bid
 enum BidPriceSpace {
-    Zero,
-    OneWei,
-    AtClearingPrice,
-    OneTickAboveClearing,
-    ManyTicksAboveClearing,
-    Max
+    // Zero,
+    // OneWei,
+    // AtClearingPrice
+    OneTickAboveClearing // First legit price to enter the auction
+        // ManyTicksAboveClearing,
+        // Max
+
 }
 
+/// @dev The amount of auctioned tokens remaining in the auction
 enum RemainingSupplySpace {
-    None,
-    All
+    // None,
+    All // all tokens remaining
+
 }
 
+/// @dev The action being performed by the sender
 enum ActionSpace {
-    SubmitBid,
-    SubmitBidWithHint,
-    ExitBid,
-    ClaimTokens,
-    Checkpoint
+    SubmitBid // enter the auction with a bid
+        // SubmitBidWithHint,
+        // ExitBid,
+        // ClaimTokens,
+        // Checkpoint
+
 }
 
+/// @dev The rate of emission in the current step of the auctioned tokens
 enum EmissionRateSpace {
-    Zero,
-    Small,
-    Normal,
-    Max
+    // Zero,
+    // Small,
+    Normal // a generic rate of emission
+        // Max
+
 }
 
+/// @dev The duration of emission in the current step of the auctioned tokens
 enum EmissionDurationSpace {
-    SingleBlock,
-    Short,
-    Normal,
-    Long
+    // SingleBlock,
+    // Short,
+    Normal // a generic duration of emission
+        // Long
+
 }
 
+/// @dev The number of emission steps in the auction
 enum NumberOfStepsSpace {
-    Zero,
-    Small,
-    Normal,
-    Max
+    // Zero,
+    // Small,
+    Normal // a generic number of emission steps
+        // Max
+
 }
 
+/// @dev The sender of the bid
 enum SenderSpace {
-    NewBidder,
-    RepeatBidder
+    NewBidder // bidder has not previously submitted a bid
+        // RepeatBidder
+
 }
 
+/// @dev The type of validation hook being used in the auction
 enum ValidationHookSpace {
-    None,
-    Reverting,
-    RevertingWithCustomError,
-    OutOfGas,
-    Passing
+    None // no validation hook attached to the auction
+        // Reverting,
+        // RevertingWithCustomError,
+        // OutOfGas,
+        // Passing
+
 }
 
+/// @dev The recipient of the auctioned tokens
 enum TokensRecipientSpace {
-    None,
-    EOA,
-    Contract
+    // None,
+    EOA // EOA is the recipient of the auctioned tokens
+        // Contract
+
 }
 
+/// @dev The recipient of the raised currency from the auction
 enum FundsRecipientSpace {
-    None,
-    EOA,
-    Contract
+    // None,
+    EOA // EOA is the recipient of the raised currency
+        // Contract
+
 }

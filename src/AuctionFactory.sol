3979b542b1e682e58f6e3bd5c8b7e94c5df455a9
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2025-10-17 00:25:00 +0000

    chore: auction factory initialize distribution test

--- src/AuctionFactory.sol
+++ src/AuctionFactory.sol
@@ -41,9 +41,9 @@ contract AuctionFactory is IAuctionFactory {
         if (amount > type(uint128).max) revert InvalidAmount(amount);
         AuctionParameters memory parameters = abi.decode(configData, (AuctionParameters));
         // If the tokensRecipient is address(1), set it to the msg.sender
-        if (parameters.tokensRecipient == ActionConstants.MSG_SENDER) parameters.tokensRecipient = msg.sender;
+        if (parameters.tokensRecipient == ActionConstants.MSG_SENDER) parameters.tokensRecipient = sender;
         // If the fundsRecipient is address(1), set it to the msg.sender
-        if (parameters.fundsRecipient == ActionConstants.MSG_SENDER) parameters.fundsRecipient = msg.sender;
+        if (parameters.fundsRecipient == ActionConstants.MSG_SENDER) parameters.fundsRecipient = sender;
 
         bytes32 initCodeHash =
             keccak256(abi.encodePacked(type(Auction).creationCode, abi.encode(token, uint128(amount), parameters)));

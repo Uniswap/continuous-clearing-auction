177743796c9b02647e9f1a3f09fed23c9ac56b18
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2025-10-22 13:06:37 +0200

    test: explicit check effective bid for 0 remaining mps.

--- test/btt/libraries/bidlib/toEffectiveAmount.t.sol
+++ test/btt/libraries/bidlib/toEffectiveAmount.t.sol
@@ -5,11 +5,28 @@ import {BttBase} from 'btt/BttBase.sol';
 import {Bid, BidLib} from 'twap-auction/libraries/BidLib.sol';
 import {ConstantsLib} from 'twap-auction/libraries/ConstantsLib.sol';
 
+import {MockBidLib} from 'btt/mocks/MockBidLib.sol';
 import {FixedPoint96} from 'twap-auction/libraries/FixedPoint96.sol';
-import {ValueX7} from 'twap-auction/libraries/ValueX7Lib.sol';
 
 contract ToEffectiveAmountTest is BttBase {
-    function test_WhenCalledWithBid(uint24 _startCumulativeMps, uint128 _amount) external pure {
+    MockBidLib internal mockBidLib;
+
+    function setUp() external {
+        mockBidLib = new MockBidLib();
+    }
+
+    function test_WhenRemainingMpsEQ0(Bid memory _bid) external {
+        // it reverts with {MpsRemainingIsZero}
+
+        // We should not hit this case ever, but it fails explicitly instead of
+        // due to division by zero
+        _bid.startCumulativeMps = ConstantsLib.MPS;
+
+        vm.expectRevert(BidLib.MpsRemainingIsZero.selector);
+        mockBidLib.toEffectiveAmount(_bid);
+    }
+
+    function test_WhenRemainingMpsNEQ0(uint24 _startCumulativeMps, uint128 _amount) external view {
         // it returns bid.amount * mps / (mps - bid.startCumulativeMps)
 
         uint24 startCumulativeMps = uint24(bound(_startCumulativeMps, 0, ConstantsLib.MPS - 1));
@@ -19,7 +36,7 @@ contract ToEffectiveAmountTest is BttBase {
         bid.startCumulativeMps = startCumulativeMps;
         bid.amountQ96 = amountQ96;
 
-        uint256 result = BidLib.toEffectiveAmount(bid);
+        uint256 result = mockBidLib.toEffectiveAmount(bid);
         assertEq(result, amountQ96 * ConstantsLib.MPS / (ConstantsLib.MPS - startCumulativeMps));
     }
 }

f32390a23030ea3fb0f988ce07a5c6c289dc1ec4
Eric Zhong <eric.zhong@uniswap.org>
2025-11-03 13:29:47 -0500

    Added unit test for unchecked math branch

--- test/Auction.submitBid.t.sol
+++ test/Auction.submitBid.t.sol
@@ -6,10 +6,13 @@ import {IAuction} from '../src/interfaces/IAuction.sol';
 import {AuctionParameters} from '../src/interfaces/IAuction.sol';
 import {Bid, BidLib} from '../src/libraries/BidLib.sol';
 import {Checkpoint} from '../src/libraries/CheckpointLib.sol';
+import {ConstantsLib} from '../src/libraries/ConstantsLib.sol';
+import {FixedPoint96} from '../src/libraries/FixedPoint96.sol';
 import {ValueX7} from '../src/libraries/ValueX7Lib.sol';
 import {AuctionBaseTest} from './utils/AuctionBaseTest.sol';
 import {FuzzBid, FuzzDeploymentParams} from './utils/FuzzStructs.sol';
 import {console2} from 'forge-std/console2.sol';
+import {FixedPointMathLib} from 'solady/utils/FixedPointMathLib.sol';
 
 contract AuctionSubmitBidTest is AuctionBaseTest {
     using BidLib for *;
@@ -45,5 +48,68 @@ contract AuctionSubmitBidTest is AuctionBaseTest {
         _maxPrice = helper__roundPriceDownToTickSpacing(_maxPrice, params.tickSpacing);
         vm.expectRevert(IAuction.InvalidBidPriceTooHigh.selector);
         auction.submitBid{value: 1}(_maxPrice, 1, alice, params.floorPrice, bytes(''));
+    }
+
+    // Rationale:
+    // This test is to verify that the auction will prevent itself from getting into a state where
+    // the unchecked math in Auction.sol:_sellTokensAtClearingPrice 202-203 below
+    //          unchecked {
+    //              totalCurrencyForDeltaQ96X7 = (uint256(TOTAL_SUPPLY) * priceQ96) * deltaMpsU;
+    //          }
+    // would cause an overflow.
+    // To hit this case, we create an auction with a very small total supply and submit bids at the
+    // maximum allowable price.
+    function test_WhenBidMaxPriceWouldCauseTotalSupplyTimesMaxPriceTimesMPSToOverflow(FuzzDeploymentParams memory _deploymentParams)
+        public
+        givenFullyFundedAccount
+    {
+        // it does not overflow in TOTAL_SUPPLY * priceQ96 * ConstantsLib.MPS
+
+        // Given that the v4 max tick price is lower bounded by 2^223, that is the max, max bid price
+        // for higher totalSupply values the max price will be lower
+        // roughly the math works out to (s2^256 - 1) / 2^223 * 2^24 < 2^9 = 512
+        // and accounting for rounding, etc. we find that the boundary is at 429
+        _deploymentParams.totalSupply = 429;
+        vm.assume(_deploymentParams.auctionParams.floorPrice > 2);
+        // Set tick spacing to 3 for this test since we know that v4 max tick is divisible by 3
+        _deploymentParams.auctionParams.tickSpacing = 3;
+        _deploymentParams.numberOfSteps = 1;
+        setUpAuction(_deploymentParams);
+
+        // Assert that the auction was setup correctly
+        assertTrue(auction.startBlock() + 1 == auction.endBlock(), 'start block + 1 should be equal to end block');
+
+        uint256 maxPrice = auction.MAX_BID_PRICE();
+        maxPrice = helper__roundPriceDownToTickSpacing(maxPrice, params.tickSpacing);
+        // Assert that we didn't fall below after rounding down to tick spacing
+        assertEq(
+            maxPrice,
+            auction.MAX_BID_PRICE(),
+            'maxPrice is not equal to MAX_BID_PRICE after rounding down to tick spacing'
+        );
+
+        // Roll to the last block to maximize `deltaMpsU` term to be 1e7
+        vm.roll(auction.endBlock() - 1);
+        uint256 totalSupply = auction.totalSupply();
+        uint256 bidAmount = FixedPointMathLib.fullMulDivUp(1, maxPrice, FixedPoint96.Q96);
+        assertLt(bidAmount, type(uint128).max, 'bidAmount would cause overflow');
+
+        // with the high max price, we can only submit bids in small increments since bidAmount is uint128
+        // submit all except for one to put us at the boundary of overflow
+        for (uint256 i = 0; i < totalSupply; i++) {
+            auction.submitBid{value: bidAmount}(maxPrice, uint128(bidAmount), alice, params.floorPrice, bytes(''));
+        }
+
+        // Assert that the next bid for 1 token unit would cause overflow, but it is caught in submit bid
+        // The amount is 1, the smallest amount of tokens that can be bid for.
+        // The maxPrice is still valid because maxPrice > clearing price.
+        bidAmount = FixedPointMathLib.fullMulDivUp(1, maxPrice, FixedPoint96.Q96);
+        vm.expectRevert(IAuction.InvalidBidUnableToClear.selector);
+        auction.submitBid{value: bidAmount}(maxPrice, uint128(bidAmount), alice, params.floorPrice, bytes(''));
+
+        // Ensure that the auction can finish and checkpoint exactly at the max bid price
+        vm.roll(auction.endBlock());
+        Checkpoint memory checkpoint = auction.checkpoint();
+        assertEq(checkpoint.clearingPrice, maxPrice, 'checkpoint clearing price is not equal to max price');
     }
 }

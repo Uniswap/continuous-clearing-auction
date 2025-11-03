c2ef126f0f11b8d9d854cb7002f24c28a05a09f4
Eric Zhong <eric.zhong@uniswap.org>
2025-11-03 18:09:48 -0500

    comment

--- src/libraries/ConstantsLib.sol
+++ src/libraries/ConstantsLib.sol
@@ -17,7 +17,7 @@ library ConstantsLib {
     ///      we must use a lower bound based on the max liquidity per tick defined in Uniswap v4,
     ///      given the smallest possible tick spacing of 1, the max liquidity per tick (L_max) is 2^107.
     ///
-    ///      Given that L_max = L_0 + L_1, where L_0 is the amount0 liquidity and L_1 is the amount1 liquidity,
+    ///      Given that L_max = min(L_0, L_1), where L_0 is the amount0 liquidity and L_1 is the amount1 liquidity,
     ///      we can express L in terms of amount0 and amount1, as well as some price bound `k`: [2^-k, 2^k]
     ///         amount0 <= L_max * (sqrt(P_max) - sqrt(k)) / (sqrt(P_max) * sqrt(k))
     ///                 <= L_max * (P_max^(-1/2) - k^(-1/2))
@@ -33,8 +33,9 @@ library ConstantsLib {
     ///        k <= 14
     ///      Therefore, our price range is [2^-14 to 2^14].
     ///
-    ///      For a total supply of 1, this will be max price. For the MAX_TOTAL_SUPPLY, this will be a max pice of 2^14, or 8192.
-    ///      This is low and as such launchers and users should be aware that in the case where the currency is less valuable
-    ///      than the token and the total supply is very large, the clearing price will be low.
+    ///      Since price is in Q96 form we need to multiply 2^107 by 2^96 before dividing by the total supply.
+    ///      If total supply is 1, the max price will be 2^203. The price will be 2^14 * 2^96 = 2^110 when total supply == MAX_TOTAL_SUPPLY.
+    ///      Integrators and users should be aware that in the case where the currency is less valuable
+    ///      than the token and the total supply is very large, the clearing price will be capped at a low price.
     uint256 constant MAX_BID_PRICE = 1 << 203;
 }

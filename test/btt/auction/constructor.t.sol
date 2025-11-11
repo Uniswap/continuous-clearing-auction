f989bcf61daa533def48d733ef214e6bf3e8918f
Eric Zhong <eric.zhong@uniswap.org>
2025-11-11 18:21:31 -0500

    fix: limit currency raised to uint128 (#278)
    * fix: limit currency raised to 128 bits
    
    * Add test that currencyRaised is less than 128 max
    
    * chore: update comment graph
    
    * chore: add labels

--- test/btt/auction/constructor.t.sol
+++ test/btt/auction/constructor.t.sol
@@ -145,6 +145,7 @@ contract ConstructorTest is BttBase {
 
         // Calculate the currency amount if all of the supply was sold at the clearing price
         uint256 currencyAmount = FixedPointMathLib.fullMulDiv(_params.totalSupply, clearingPrice, FixedPoint96.Q96);
+        assertLe(currencyAmount, type(uint128).max, 'currencyAmount is greater than type(uint128).max');
 
         // If currency is currency0, we need to invert the price (price = currency1/currency0)
         uint256 temp;

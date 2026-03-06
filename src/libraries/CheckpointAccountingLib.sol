3f10a5dded7bd8b143814a51921bec6fd377d859
Eric Zhong <eric.zhong@uniswap.org>
2026-03-06 15:48:34 -0500

    save

--- src/libraries/CheckpointAccountingLib.sol
+++ src/libraries/CheckpointAccountingLib.sol
@@ -71,10 +71,11 @@ library CheckpointAccountingLib {
         pure
         returns (uint256 tokensFilled, uint256 currencySpentQ96)
     {
+        if (cumulativeMpsPerPriceDelta == 0) return (0, 0);
         uint24 mpsRemainingInAuctionAfterSubmission = bid.mpsRemainingInAuctionAfterSubmission();
 
         // Currency spent is original currency amount multiplied by percentage fully filled over percentage allocated
-        currencySpentQ96 = bid.amountQ96.fullMulDivUp(cumulativeMpsDelta, mpsRemainingInAuctionAfterSubmission);
+        currencySpentQ96 = bid.amountQ96.fullMulDivUp((FixedPoint96.Q96 << FixedPoint96.RESOLUTION) * mpsRemainingInAuctionAfterSubmission, cumulativeMpsPerPriceDelta);
 
         // Tokens filled are calculated from the effective amount over the allocation
         tokensFilled = bid.amountQ96

5f5dd386f4be350d8cfe8c29e2027e558df20f25
Eric Zhong <eric.zhong@uniswap.org>
2026-03-18 14:18:16 -0400

    chore: fix canClearSupplyAtPrice test

--- test/btt/libraries/demandLib/canClearSupplyAtPrice.t.sol
+++ test/btt/libraries/demandLib/canClearSupplyAtPrice.t.sol
@@ -5,6 +5,7 @@ import {Math} from '@openzeppelin/contracts/utils/math/Math.sol';
 import {BttBase} from 'btt/BttBase.sol';
 import {ConstantsLib} from 'continuous-clearing-auction/libraries/ConstantsLib.sol';
 import {DemandLib} from 'continuous-clearing-auction/libraries/DemandLib.sol';
+import {FixedPoint96} from 'continuous-clearing-auction/libraries/FixedPoint96.sol';
 
 contract CanClearSupplyAtPriceTest is BttBase {
     function test_WhenDemandTimesRemainingMps_GT_RemainingSupply(
@@ -20,7 +21,7 @@ contract CanClearSupplyAtPriceTest is BttBase {
         uint256 priceQ96 = bound(_priceQ96, 1, ConstantsLib.X7_UPPER_BOUND);
         uint256 remainingMps = bound(_remainingMps, 1, ConstantsLib.MPS);
 
-        (uint256 highLhs, uint256 lowLhs) = Math.mul512(demandQ96 * remainingMps, priceQ96);
+        (uint256 highLhs, uint256 lowLhs) = Math.mul512(demandQ96 * remainingMps, FixedPoint96.Q96);
         (uint256 highRhs, uint256 lowRhs) = Math.mul512(remainingSupplyQ96X7, priceQ96);
 
         vm.assume(highLhs > highRhs || (highLhs == highRhs && lowLhs > lowRhs));
@@ -28,17 +29,16 @@ contract CanClearSupplyAtPriceTest is BttBase {
         assertTrue(DemandLib.canClearSupplyAtPrice(demandQ96, remainingSupplyQ96X7, priceQ96, remainingMps));
     }
 
-    function test_WhenDemandTimesRemainingMps_EQ_RemainingSupply(uint256 _remainingSupplyQ96X7, uint256 _priceQ96)
-        external
-        pure
-    {
+    function test_WhenDemandTimesRemainingMps_EQ_RemainingSupply(uint256 _remainingSupplyQ96X7) external pure {
         // it returns true
 
         uint256 remainingSupplyQ96X7 = bound(_remainingSupplyQ96X7, 0, ConstantsLib.X7_UPPER_BOUND);
-        uint256 priceQ96 = bound(_priceQ96, 1, ConstantsLib.X7_UPPER_BOUND);
 
-        // demand * remainingMps == remainingSupply when demand == remainingSupply and remainingMps == 1
+        // For simplicitly, assume price is Q96
+        uint256 priceQ96 = FixedPoint96.Q96;
+        // And that demand == supply
         uint256 demandQ96 = remainingSupplyQ96X7;
+        // And that there is 1 mps remaining
         uint256 remainingMps = 1;
 
         assertTrue(DemandLib.canClearSupplyAtPrice(demandQ96, remainingSupplyQ96X7, priceQ96, remainingMps));
@@ -57,7 +57,7 @@ contract CanClearSupplyAtPriceTest is BttBase {
         uint256 priceQ96 = bound(_priceQ96, 1, ConstantsLib.X7_UPPER_BOUND);
         uint256 remainingMps = bound(_remainingMps, 1, ConstantsLib.MPS);
 
-        (uint256 highLhs, uint256 lowLhs) = Math.mul512(demandQ96 * remainingMps, priceQ96);
+        (uint256 highLhs, uint256 lowLhs) = Math.mul512(demandQ96 * remainingMps, FixedPoint96.Q96);
         (uint256 highRhs, uint256 lowRhs) = Math.mul512(remainingSupplyQ96X7, priceQ96);
 
         vm.assume(highLhs < highRhs || (highLhs == highRhs && lowLhs < lowRhs));
